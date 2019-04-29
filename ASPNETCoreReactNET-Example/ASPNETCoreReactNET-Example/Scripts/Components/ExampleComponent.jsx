7da868a4a693b641ed07d1c26d2214d376b0fcb1
Martin Teichler <teichler_martin@outlook.de>
2019-04-29 18:01:34 +0200

    Added files

---
+++ ASPNETCoreReactNET-Example/ASPNETCoreReactNET-Example/Scripts/Components/ExampleComponent.jsx
@@ -0,0 +1,11 @@
+﻿class ExampleComponent extends React.Component {
+    render() {
+        return (
+            <div className="testDiv">
+                <TextField label="With error message" errorMessage="Error message" />
+                <h1>{this.props.user.name}</h1>
+                <h3>{this.props.user.age}</h3>   
+            </div>
+        );
+    }
+}
---
+++ ASPNETCoreReactNET-Example/ASPNETCoreReactNET-Example/Scripts/main.js
@@ -0,0 +1,4 @@
+﻿// ReactJS.NET needs to expose components globally, so we do this
+var Components = require('expose-loader?Components!./components');
+var react = require('expose-loader?react!react');
+var OUI = require('expose-loader?office-ui-fabric-react!office-ui-fabric-react');
